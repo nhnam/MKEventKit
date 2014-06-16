07b950c0e1a1b451c9496b9060fbb186a47c9e3e
Michal Konturek <michal.konturek@gmail.com>
2014-06-16 23:12:40 +0100

    Initial commit.

---
+++ MKEventKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MKEventKit
+//
+//  Created by Michal Konturek on 16/06/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
